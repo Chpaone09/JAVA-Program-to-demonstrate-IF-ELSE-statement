71dbfe1a28d16e1b20f0552a7e2e576b6349240d
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-11 22:19:24 +0530

    Program to demonstrate IF-ELSE Statement in java

---
+++ IfElse.java
@@ -0,0 +1,20 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate IF-ELSE Statement in java.
+Author  : Chpaone09®
+Date    : Oct 11, 2020
+
+************************************************************
+*/
+
+class IfElse{
+   public static void main(String[] args) {
+        int a = 16;
+       if((a%2)==0){
+            System.out.println("\n\n\t "+a+" is EVEN ");
+       }else{
+            System.out.println("\n\n\t "+a+" is ODD ");
+       }
+   }
+}
